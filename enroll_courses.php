c01d2b45b8aef9c1ef82cee1b6b26fe05bfd456d
ameerah96 <ameerah.nsultan@gmail.com>
2018-09-26 22:11:17 +0300

    enroll not completed

--- enroll_courses.php
+++ enroll_courses.php
@@ -1,51 +1,70 @@
 <?php
 
 include"static_menue.html";
+session_start();
+
+//if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false)
+//{
+//echo "<META HTTP-EQUIV='Refresh' Content='1;URL=login.html'>";
+//exit() ;	
+//}
 ?>
   <body>
-    <div class="container-fluid">
-      <div class="row">
-        
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-         <h1 class="page-header">Course you can enroll are:</h1>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+				<h1 class="page-header">Course you can enroll are:</h1>
 			</div>
-          <div id='courseNameDiv' class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-		  <table id ="mytable" width='50%' border=1px solid black> 
-		  <thead> <th> Course Name</th><th> credit Hours</th></thead>
-		  <tbody>	
-        </div>
-      </div>
+			<div id='courseNameDiv' class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+				<table id ="mytable" width='50%' border=1px solid black> 
+					<thead> <th> Course Name</th><th> Credit Hours</th><th>Sections</th></thead>
+					<tbody>	
+			</div>
+		</div>
     </div>
-
    <script type='text/javascript'>
 
-var xmlhttp = new XMLHttpRequest();
-var content ='<p> i am here</p>';
-xmlhttp.onreadystatechange = function(){
-	if(this . readyState == 4 && this . status == 200)
-  {
-	var courseName = JSON.parse(this .responseText);
-	var table = document.getElementById("mytable");	
-
-	for(var x=0; x< courseName.length; x++)
-	{	
-		var row = table.insertRow(-1);		
-		for (var i=0; i< courseName[x].length; i++)
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function()
+	{
+		if(this . readyState == 4 && this . status == 200)
 		{
-		var cell = row.insertCell(i);
-		cell.innerHTML = "<a href='enroll_course_form.php'>"+courseName[x][i]+"</a>";
-	// may i will use this later
-	/*	var anchortags = document.querySelectorAll("a[href*='#']");
-		anchortags.forEach(function(tag) {
-		tag.href = tag.href.replace('&lanekilde=', '');
-    });*/
-		}
-	}
-  }// end if
-}// end function 
+			var courseName = JSON.parse(this .responseText);
+			var table = document.getElementById("mytable");	
+			var content =" ";
+			// the need of 2 for loop because the courseName will contain many array ..each array contains the info of the course with the sections
+			for(var x=0; x< courseName.length; x++)
+			{		
+				// -1 will add the row at the end of the table 
+				var row = table.insertRow(-1);		
+				for (var i=0; i< courseName[x].length; i++)
+				{
+					var cell = row.insertCell(i);
+					if(courseName[x][i].constructor === Array)
+					{
+						array = courseName[x][i];
+						
+						for (var q=0;q<array.length; q++)
+						{   
+							for(var key in array[q])
+								cell.innerHTML +="<a id='enroll' href='enroll_course_form.php'>"+ key +" : " +array[q][key] + " , ";	
+						}
+					}
+					else
+					{
+					cell.innerHTML = "<a href='enroll_course_form.php'>"+courseName[x][i]+"</a>";
+					}
+					// may i will use this later
+					/*	var anchortags = document.querySelectorAll("a[href*='#']");
+					anchortags.forEach(function(tag) {
+					tag.href = tag.href.replace('&lanekilde=', ''););*/
+				}
+			}
+		}// end if
+	}// end function 
 	xmlhttp.open("POST","view_enroll.php",true);
 	xmlhttp.send();	
 
 </script>
-  </body>
+</body>
 </html>
--- view_enroll.php
+++ view_enroll.php
@@ -6,80 +6,77 @@ session_start();
 //echo "<META HTTP-EQUIV='Refresh' Content='1;URL=login.html'>";
 //exit() ;	
 //}
-?>
-<?php
 require('db.php');
-$try = array();
 $time = array();
 $possible_courses = [];
-$GLOBALS["i"]=0;
 $sql = 'SELECT ID_course, name,credits_hours FROM course';
 $result = mysqli_query($link, $sql);
 $rows = 0;
 $rows = mysqli_num_rows($result);
+
 if ($rows > 0) 
 {
-    // output data of each row
-    while($row = $result->fetch_assoc())
+	// output data of each row
+	while($row = mysqli_fetch_assoc($result))
 	{
-		// get sections of this course 
-		$sections = $link->query('select section_ID,timeslot_ID,room_no from section where course_ID =$row["ID_course"]');
+		$sections = $link->query('select section_ID,timeslot_ID,room_no from section where course_ID ='.$row["ID_course"]);
 		//get time and date
 		if ($sections)
 		{
-		while($section = $sections->fetch_assoc())
-		{
-		$section_time = $link->query('select * from time_slot where timeslot_ID = $section["timeslot_ID"]');
-		array_push($time,$section_time);
-		
-		}
-		}
+			while($section = mysqli_fetch_assoc($sections))
+			{	
+				$section_time = $link->query('select days,start_time,end_time from time_slot where timeslot_ID ='. $section["timeslot_ID"]);
+				// push each time slot of the section to the  array time
+				while($s = mysqli_fetch_assoc($section_time))
+				{
+					$s["section_ID"]= $section["section_ID"];
+					array_push($time,$s);
+				}// end while 
+			}//end while
+		}// end if
 		// check if the course is taken before or not 
 		// if it is taken so check status of it 
 		// if the status fail or dropped then the student can enroll
-		$exists = $link->query('select ID from enroll_course where ID_course = $row["ID_course"] and ID_student =$_POST["student_ID"] ');
+		$exists = $link->query('select ID from enroll_course where ID_course = $row["ID_course"] and ID_student =$_POST["student_ID"]');
         if( $exists ) 
 		{
 			$status = $link->query('select status from enroll_course where ID =$exists["ID"]');
-		   if ($status == "fail" or $status == "dropped")
-		   {
-			   array_push($possible_courses,array($row["name"],$row["credits_hours"]));
-			   //$possible_courses[$GLOBALS["i"]] = array("name"=>$row["name"],"credit hours"=>$row["credits_hours"]);
-				//$GLOBALS["i"] = $GLOBALS["i"] + 1 ;
-		   }
-		   // if the status not pass and not current then the student dosen't take the course but check the requisite
-		   if ($status != "current" and $status !="pass")
+			if ($status == "fail" or $status == "dropped")
+		    { 
+				array_push($possible_courses,array($row["name"],$row["credits_hours"],$time));
+				// reset array of the time to add another course with its own sections
+				$time = array();
+			}
+			// if the status not pass and not current then the student dosen't take the course but check the requisite
+			if ($status != "current" and $status !="pass")
 			{
-			$requisite =$link->query('select requisite_ID from requisite where course_ID = $row["ID_course"] and type = "pre"');
+				$requisite =$link->query('select requisite_ID from requisite where course_ID = $row["ID_course"] and type = "pre"');
 				if( $requisite)
 				{
 					$exists = $link->query('select ID from enroll_course where ID_course = $requisite["requisite_ID"] and ID_student =$_POST["ID_student"] ');
 					if( $exists )
 					{
-					$status = $link->query('select status from enroll_course where ID = ID =$exists["ID"]');
-					if ($status == "pass")
-					{
-						array_push($possible_courses,array($row["name"],$row["credits_hours"]));
-				//$possible_courses[$GLOBALS["i"]] = array("name"=>$row["name"],"credit hours"=>$row["credits_hours"]);
-				//$GLOBALS["i"] = $GLOBALS["i"] + 1 ;
-					}	
+						$status = $link->query('select status from enroll_course where ID = ID =$exists["ID"]');
+						if ($status == "pass")
+						{
+							array_push($possible_courses,array($row["name"],$row["credits_hours"],$time));
+							// reset array of the time to add another course with its own sections
+							$time = array();
+						}	
 					}			
 				}
 			}
-		
 		}
-		array_push($possible_courses,array($row["name"],$row["credits_hours"]));
-				//$possible_courses[$GLOBALS["i"]] = array("name"=>$row["name"],"credit hours"=>$row["credits_hours"]);
-				//$GLOBALS["i"] = $GLOBALS["i"] + 1 ;		
+		array_push($possible_courses,array($row["name"],$row["credits_hours"],$time));
+		// reset array of the time to add another course with its own sections
+		$time = array();
     }
 	$course_name_json= json_encode($possible_courses);
-echo $course_name_json;
+	echo $course_name_json;
 } 
-//	print_r( json_encode($possible_courses));
-//header('Content-Type: application/json');
-else {
+else{
     echo "0 results";
-}
+	}
 ?>
 
 
